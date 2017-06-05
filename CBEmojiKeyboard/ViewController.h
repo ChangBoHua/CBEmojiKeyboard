b2a5da28c895a94dde22c15abfec06cefda5f533
昶博 <you@example.com562088039@qq.com>
2017-06-05 15:29:46 +0800

    Initial Commit

---
+++ CBEmojiKeyboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CBEmojiKeyboard
+//
+//  Created by 俊欧巴 on 17/6/5.
+//  Copyright © 2017年 俊欧巴. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CBEmojiKeyboardTests/CBEmojiKeyboardTests.m
@@ -0,0 +1,39 @@
+//
+//  CBEmojiKeyboardTests.m
+//  CBEmojiKeyboardTests
+//
+//  Created by 俊欧巴 on 17/6/5.
+//  Copyright © 2017年 俊欧巴. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CBEmojiKeyboardTests : XCTestCase
+
+@end
+
+@implementation CBEmojiKeyboardTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
